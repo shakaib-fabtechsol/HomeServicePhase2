fd7a80a862c52a25a4076a23893e244489c4b7d5
Nadia-FTS <nadia@fabtechsol.com>
2025-02-17 12:33:37 +0500

    tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import UserLayout from "./Layouts/UserLayout";
 import Dashboard from "./Pages/User/Dashboard";
+import Notification from "./Pages/User/Notification";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route element={<UserLayout />}>
           <Route path="/user/dashboard" element={<Dashboard />} />
+          <Route path="/user/notification" element={<Notification />} />
         </Route>
       </Routes>
     </Router>
