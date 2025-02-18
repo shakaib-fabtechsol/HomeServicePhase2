f64bc4c447dc2c41dd4a753c2fe4139d4afecfb2
Nadia-FTS <nadia@fabtechsol.com>
2025-02-18 17:01:44 +0500

    tabs

--- src/Components/TabComponent.jsx
+++ src/Components/TabComponent.jsx
@@ -42,31 +42,32 @@ export default function TabComponent({ tabs }) {
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
+      <Box sx={{ borderBottom: 0, borderColor: 'divider' }}>
         <Tabs indicatorColor='transparent'
-        sx={{fontFamily:"inter", minHeight: "32px",}} value={value} onChange={handleChange} aria-label="custom tabs example">
+          sx={{ fontFamily: "inter", minHeight: "32px", }} value={value} onChange={handleChange} aria-label="custom tabs example">
           {tabs.map((tab, index) => (
             <Tab sx={{
-                fontFamily:"inter",
-                textTransform: "capitalize",
-                fontSize: "16px",
-                fontWeight: "400",
-                color: "#000",
-                minWidth: "42px",
-                marginRight: "15px",
-                position: "relative",
-                minHeight: "32px",
-                padding: "5px 14px",
-                '&.Mui-selected': {
-                  borderBottom: "2px solid #0F91D2",
-                  fontWeight: "600",
-                },
+              fontFamily: "inter",
+              textTransform: "capitalize",
+              fontSize: "16px",
+              fontWeight: "400",
+              color: "#000",
+              minWidth: "42px",
+              marginRight: "15px",
+              position: "relative",
+              minHeight: "32px",
+              padding: "5px 14px",
+              '&.Mui-selected': {
+                color: "#0F91D2 !important",
+                borderBottom: "solid 4px #0F91D2",
+                fontFamily: "inter",
+              },
             }} key={index} label={tab.label} {...a11yProps(index)} />
           ))}
         </Tabs>
       </Box>
       {tabs.map((tab, index) => (
-        <CustomTabPanel sx={{fontFamily:"inter"}} key={index} value={value} index={index}>
+        <CustomTabPanel sx={{ fontFamily: "inter" }} key={index} value={value} index={index}>
           {tab.content}
         </CustomTabPanel>
       ))}
--- src/Pages/ServiceProvider/NewDeals.jsx
+++ src/Pages/ServiceProvider/NewDeals.jsx
@@ -1,41 +1,19 @@
 import React, { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import BasicInfo from "../../Components/Services.jsx/BasicInfo";
 import PricingPackaging from "../../Components/Services.jsx/PricingPackaging";
 import MediaUpload from "../../Components/Services.jsx/MediaUpload";
 import ReviewPublish from "../../Components/Services.jsx/ReviewPublish";
-
-function TabPanel(props) {
-  useEffect(() => {
-    document.title = "New Deals";
-  }, []);
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ py: 3, fontFamily: "inter" }}>{children}</Box>
-      )}
-    </div>
-  );
-}
+import TabComponent from "../../Components/TabComponent";
 
 function NewDeals() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const tabData = [
+    { label: "Basic Info", content: <BasicInfo/> },
+    { label: "Pricing & Packages", content: <PricingPackaging /> },
+    { label: "Media Upload", content: <MediaUpload /> },
+    { label: "Review & Publish", content: <ReviewPublish /> },
+  ];
 
   return (
     <div>
@@ -46,47 +24,7 @@ function NewDeals() {
         <h2 className="text-2xl font-semibold">Create New Deal</h2>
       </div>
       <p className="text-[#535862] mt-4 ms-8">Create, manage, and organize your deals effortlessly.</p>
-      <Box sx={{ width: "100%", typography: "body1", marginTop: "20px" }}>
-        <Box>
-          <Tabs
-            TabIndicatorProps={{ sx: { display: "none" } }}
-            sx={{
-              "& .MuiTab-root": {
-                color: "#717680",
-                fontWeight: "600",
-                fontFamily: "inter",
-              },
-              "& .Mui-selected": {
-                color: "#0F91D2 !important",
-                borderBottom: "solid 4px #0F91D2",
-                fontFamily: "inter",
-              },
-            }}
-            value={value}
-            onChange={handleChange}
-            variant="scrollable"
-            scrollButtons="auto" 
-          >
-            <Tab label="Basic Info" />
-            <Tab label="Pricing & Packages" />
-            <Tab label="Media Uploads" />
-            <Tab label="Review & Publish" />
-          </Tabs>
-        </Box>
-
-        <TabPanel value={value} index={0}>
-          <BasicInfo />
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <PricingPackaging />
-        </TabPanel>
-       <TabPanel value={value} index={2}>
-          <MediaUpload />
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          <ReviewPublish />
-        </TabPanel>
-      </Box>
+      <TabComponent tabs={tabData}/>
     </div>
   );
 }
