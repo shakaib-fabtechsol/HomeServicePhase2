f780a9e1ece718a327b9d2466ece27fb5a8bea4f
Nadia-FTS <nadia@fabtechsol.com>
2025-02-20 17:40:13 +0500

    acc

--- src/Pages/Customer/Profile.jsx
+++ src/Pages/Customer/Profile.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { FaArrowLeft } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import AccordionComponent from '../../Components/AccordionComponent'
 
 const Profile = () => {
+  const accordionData = [
+    { title: 'Item #1', content: 'Content for Item #1 goes here.' },
+    { title: 'Item #2', content: 'Content for Item #2 goes here.' },
+    { title: 'Item #3', content: 'Content for Item #3 goes here.' },
+  ];
   return (
-    <div>Profile</div>
+    <div>
+        <div className="flex items-center">
+          <Link to="#"><FaArrowLeft className="me-4 text-base" /></Link>
+          <h2 className="text-2xl font-semibold">Profile Details</h2>
+        </div>
+        <AccordionComponent items={accordionData} />
+    </div>
   )
 }
 
