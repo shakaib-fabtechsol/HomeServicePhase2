286441136fe66e64d091f1329585b997f7f07982
mehak187 <mehakamir187@gmail.com>
2025-02-21 18:48:58 +0500

    formatting

--- src/Layouts/CustomerLayout.jsx
+++ src/Layouts/CustomerLayout.jsx
@@ -1,69 +1,90 @@
-import React, { useState } from 'react'
-import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
-import FilterNav from './FilterNav';
-import MainNav from './MainNav';
-import { IoChatboxEllipsesOutline, IoHomeOutline } from 'react-icons/io5';
-import { AiOutlineDollar, AiOutlineSetting } from 'react-icons/ai';
-import { RiBarChartHorizontalLine } from 'react-icons/ri';
-import { CiCreditCard1 } from 'react-icons/ci';
-import { FaRegHeart } from 'react-icons/fa';
-import { PiFiles } from 'react-icons/pi';
-import { GrNotification } from 'react-icons/gr';
-import { CgSupport } from 'react-icons/cg';
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import FilterNav from "./FilterNav";
+import MainNav from "./MainNav";
+import { IoChatboxEllipsesOutline, IoHomeOutline } from "react-icons/io5";
+import { AiOutlineDollar, AiOutlineSetting } from "react-icons/ai";
+import { RiBarChartHorizontalLine } from "react-icons/ri";
+import { CiCreditCard1 } from "react-icons/ci";
+import { FaRegHeart } from "react-icons/fa";
+import { PiFiles } from "react-icons/pi";
+import { GrNotification } from "react-icons/gr";
+import { CgSupport } from "react-icons/cg";
 
 const CustomerLayout = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
-    const sidebarData = [
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  const sidebarData = [
+    {
+      items: [
+        { to: "/customer/dashboard", icon: IoHomeOutline, label: "Home" },
+        { to: "/customer/bucks", icon: AiOutlineDollar, label: "Pro Bucks" },
         {
-            items: [
-                { to: "/customer/dashboard", icon: IoHomeOutline, label: "Home" },
-                { to: "/customer/bucks", icon: AiOutlineDollar, label: "Pro Bucks" },
-                { to: "/customer/order", icon: RiBarChartHorizontalLine, label: "Orders"},
-                { to: "/customer/payments", icon: CiCreditCard1, label: "Payments/Payout",},
-                { to: "/customer/favourites", icon: FaRegHeart, label: "Favorites", toChild: "/customer/dealDetails" },
-                { to: "/customer/reports", icon: PiFiles, label: "Reports" },
-            ],
+          to: "/customer/order",
+          icon: RiBarChartHorizontalLine,
+          label: "Orders",
         },
         {
-            items: [
-                { to: "/customer/message",  icon: IoChatboxEllipsesOutline, label: "Conversations",},
-                { to: "/customer/notification", icon: GrNotification, label: "Notifications",},
-                { to: "/customer/setting", icon: AiOutlineSetting, label: "Settings" },
-                { to: "/customer/support", icon: CgSupport, label: "Support" },
-            ],
+          to: "/customer/payments",
+          icon: CiCreditCard1,
+          label: "Payments/Payout",
         },
-    ];
+        {
+          to: "/customer/favourites",
+          icon: FaRegHeart,
+          label: "Favorites",
+          toChild: "/customer/dealDetails",
+        },
+        { to: "/customer/reports", icon: PiFiles, label: "Reports" },
+      ],
+    },
+    {
+      items: [
+        {
+          to: "/customer/message",
+          icon: IoChatboxEllipsesOutline,
+          label: "Conversations",
+        },
+        {
+          to: "/customer/notification",
+          icon: GrNotification,
+          label: "Notifications",
+        },
+        { to: "/customer/setting", icon: AiOutlineSetting, label: "Settings" },
+        { to: "/customer/support", icon: CgSupport, label: "Support" },
+      ],
+    },
+  ];
 
-    const userInfo = {
-        name: "Mike Bird",
-        email: "mikebird@untitledui.com",
-        profileLink: "/customer/profile"
-    };
+  const userInfo = {
+    name: "Mike Bird",
+    email: "mikebird@untitledui.com",
+    profileLink: "/customer/profile",
+  };
 
-    
-    return (
-        <div className="mainpage">
-            <MainNav toggleSidebar={toggleSidebar} />
-            <FilterNav />
-            <div className="main flex">
-                <Sidebar
-                    isSidebarOpen={isSidebarOpen}
-                    sidebarData={sidebarData}
-                    userInfo={userInfo} />
-                <div className="right">
-                    <div className="right-bottom px-2">
-                        <div className="container-fluid">
-                            <Outlet />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="mainpage">
+      <MainNav toggleSidebar={toggleSidebar} />
+      <FilterNav />
+      <div className="main flex">
+        <Sidebar
+          isSidebarOpen={isSidebarOpen}
+          sidebarData={sidebarData}
+          userInfo={userInfo}
+        />
+        <div className="right">
+          <div className="right-bottom px-2">
+            <div className="container-fluid">
+              <Outlet />
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default CustomerLayout
+export default CustomerLayout;
