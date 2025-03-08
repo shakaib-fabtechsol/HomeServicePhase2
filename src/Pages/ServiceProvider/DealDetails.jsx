d1e920aa82b2c0c9d16b45a92dae7941b5ea8ff8
talhariaz105 <muhammad.talha@fabtechsol.com>
2025-03-08 17:13:33 +0500

    Update DealDetails.jsx

--- src/Pages/ServiceProvider/DealDetails.jsx
+++ src/Pages/ServiceProvider/DealDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ServiceDetail from "../../Components/Services.jsx/ServiceDetail";
+import { useDeleteDealMutation, useGetDealQuery, useGetUserDetailsQuery } from "../../services/base-api";
 
 function DealDetails() {
   return <ServiceDetail role="provider" backto={"/provider/services"} useGetUserDetailsQuery={useGetUserDetailsQuery} useGetDealQuery={useGetDealQuery} useDeleteDealMutation={useDeleteDealMutation} />;
